7ca87944f23abf287f8fb42e990527ef296ebd57
Chaibi-Anas <anas.chaibi@insat.ucar.tn>
2024-02-25 13:24:57 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:14
+
+# create me an app folder
+WORKDIR /app
+
+# copy the package  
+COPY package*.json ./
+
+#INSTALL DEPENDICIES
+RUN npm install
+
+COPY . .
+
+EXPOSE 3000
+
+
+
--- src/app.service.ts
+++ src/app.service.ts
@@ -13,6 +13,6 @@ export class AppService {
     const apikey = this.configService.apiKey;
     const dbName = this.configService.database.name;
     const port = this.configService.database.port;
-    return `apiKey: ${apikey}, dbName: ${dbName} en el port: ${port}`;
+    return `Bonjour les Gl4 :)`;
   }
 }
